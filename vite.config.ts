5500b47cc5ba4a01cae60814d56d0b5153b670c8
zhedieya <318992691@qq.com>
2022-11-20 18:38:00 +0800

    chore:add tsx dep

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { resolve } from "path" // npm  install  path  --save
-
+import { resolve } from 'path' // npm  install  path  --save
+import vueJsx from '@vitejs/plugin-vue-jsx'
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,11 +14,14 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
   ],
   resolve: {
     // ↓路径别名，主要是这部分
     alias: {
-      "@": resolve(__dirname, "./src") // npm install --save-dev @types/node  不然找不到名称dirname
-    }
-  }
+      '@': resolve(__dirname, './src'), // npm install --save-dev @types/node  不然找不到
+    },
+  },
 })
